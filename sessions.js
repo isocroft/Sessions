ffa9c7f3c52012872a1eb731eddff18d3fddf4ef
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2014-10-22 11:00:07 +0100

    reinit

--- sessions.js
+++ sessions.js
@@ -23,71 +23,42 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-'use strict';
-
-!function(name, defs){ 
- 
-if(!defs.locked){ // define only once!!
-
-     var hOwn = ({}).hasOwnProperty;
-     // CommonJS standard --- NodeJS
-     if(typeof module != "undefined" && hOwn.call(module, "exports"))
-          module.exports = defs( {} );
-     // AMD Standard ---- RequireJS     
-     else if(typeof define == "function" && hOwn.call(define, "amd"))
-          define(function(){ return def( {} ); });
-     // Normal JS
-     else
-       this[name] = defs( {} );
-       
-    defs.locked = true;   
-}       
-
-}( "sessions", function(sessions) {
-	        
-	      
+var sessions = (function(sessions) {
 		var _idle_flag = false,
-		        d = document,
-		        lastTime,
-			_idle_counter = 0, 
+			_idle_counter = 0,
 			check_idle_time,
 			IDLE_PERIOD,
+			_idle_check,
 			idle_elem,
 			events,
-			l,
-			eventMap = ["click", "mousemove", "keypress"],
-			rgX = new RegExp("^("+eventMap.join("|")+")$"),
 			url,
-			doc_event_register = d.addEventListener || d.attachEvent;
+			run;
 
-		
-
-		events = function(e) { 
-			if (e.type.match(rgX) != null)
-				_idle_counter = 0; // any time an user becomes active, extend the time for log out!
+		_idle_check = function() {
+			if (_idle_flag) {
+				if (_idle_counter >= IDLE_PERIOD) document.location.href = url;
+			} else {
+				_idle_counter = 0;
+			}
 		};
 
-                // when you go with the below, what if the user set handlers for these event direcly on the document??
-		/** document.onclick = document.onmousemove = document.onkeypress = events; **/
-		
-		for(l = 0; l < eventMap.length; l++)
-		     doc_event_register.call(d, (? "on"+eventMap[l] : eventMap[l]), events, false);
+		events = function(e) {
+			if (e.type == "click" || e.type == "mousemove" || e.type == "keypress")
+				_idle_check();
+		};
+
+		document.onclick = document.onmousemove = document.onkeypress = events;
 
 		check_idle_time = function(elem, tm) {
-			var now = (ne Dtae).getTime();
-			    diff = now - lastTime;
-			if (elem && typeof elem.nodeType == "number") {
+			++_idle_counter;
+			if (elem !== '') {
 				elem.style.display = 'block';
 				elem.innerHTML = (IDLE_PERIOD - _idle_counter) + '';
-		        }
-		     if (_idle_counter >= IDLE_PERIOD) _idle_flag = true;
-                     
-		     if (_idle_flag) { // if time is up before the user gets a chance to become active, then log user out
-			    document.location.href = url;
-		     } 
-		     lastTime = now;
-		     _idle_counter += diff; // add how much time has passed since we started checkin --- sessions.init
-	        };
+		}
+		if (_idle_counter == IDLE_PERIOD) _idle_flag = true;
+
+		return _idle_flag;
+	};
 
 	sessions.init = function(mUrl, opt) {
 		var opt = opt || {};
@@ -95,14 +66,11 @@ if(!defs.locked){ // define only once!!
 		url = ( !! mUrl && typeof mUrl === 'string') ? mUrl : '';
 		IDLE_PERIOD = ( !! opt.time && typeof opt.time === 'number') ? opt.time : 20 * 60;
 
-		/**run = window.setTimeout(function() { **/
-			if(!check_idle_time(idle_elem, IDLE_PERIOD)){
-		             setTimeout(arguments.callee.bind(null, mUrl, opt), 0);
-			}
-		/** }, 1000); **/
+		run = window.setInterval(function() {
+			var rep = check_idle_time(idle_elem, IDLE_PERIOD);
+			if (rep) clearTimeout(run);
+		}, 1000);
 	};
-	
-	 
 
 	return sessions;
-});
+})(sessions || {});
